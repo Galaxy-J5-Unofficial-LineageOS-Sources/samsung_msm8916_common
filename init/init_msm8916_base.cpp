79bfe657294ccca42c299379205ebd3dc9d2707c
David <davidmolinamorales1999@gmail.com>
2021-10-09 17:02:07 +0200

    msm8916-common: init: set Pixel 2 fingerprint to bypass Safetynet

--- init/init_msm8916_base.cpp
+++ init/init_msm8916_base.cpp
@@ -105,25 +105,10 @@ void set_wifi_properties()
 
 void set_fingerprint()
 {
-	std::string fingerprint = GetProperty("ro.build.fingerprint", "");
+   // fingerprint
+    property_override("ro.build.description", "j5lteub-user 6.0.1 MMB29M J500MUBU1BQA1 release-keys");
+    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
 
-	if ((strlen(fingerprint.c_str()) > 1) && (strlen(fingerprint.c_str()) <= PROP_VALUE_MAX))
-		return;
-
-	char new_fingerprint[PROP_VALUE_MAX+1];
-
-	std::string build_id = GetProperty("ro.build.id","");
-	std::string build_tags = GetProperty("ro.build.tags","");
-	std::string build_type = GetProperty("ro.build.type","");
-	std::string device = GetProperty("ro.product.device","");
-	std::string incremental_version = GetProperty("ro.build.version.incremental","");
-	std::string release_version = GetProperty("ro.build.version.release","");
-
-	snprintf(new_fingerprint, PROP_VALUE_MAX, "samsung/%s/%s:%s/%s/%s:%s/%s",
-			 device.c_str(), device.c_str(), release_version.c_str(), build_id.c_str(),
-			 incremental_version.c_str(), build_type.c_str(), build_tags.c_str());
-
-	property_override_dual("ro.build.fingerprint", "ro.boot.fingerprint", new_fingerprint);
 }
 
 void set_common_properties()
