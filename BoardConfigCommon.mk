9ef3fcc0c3fe4b7fa007f8cdc2b181d93a61746c
David <davidmolinamorales1999@gmail.com>
2021-11-21 11:47:12 +0100

    msm8916-common: kick frameworks_compatibility_matrix

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -126,7 +126,7 @@ AUDIO_FEATURE_ENABLED_FM_POWER_OPT := true
 
 # HIDL
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(COMMON_PATH)/framework_compatibility_matrix.xml
+#DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(COMMON_PATH)/framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 
 # Init
