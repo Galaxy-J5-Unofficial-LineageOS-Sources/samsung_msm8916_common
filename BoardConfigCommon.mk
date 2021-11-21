ab860f13cc6d88240e10e96962782c0571d2b2d0
David <davidmolinamorales1999@gmail.com>
2021-11-21 15:29:44 +0100

    msm8916-common: restore old sepolicy dir

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -212,7 +212,8 @@ TARGET_RECOVERY_PIXEL_FORMAT	:= "RGB_565"
 # SELinux
 SELINUX_IGNORE_NEVERALLOWS := true
 include device/qcom/sepolicy-legacy/sepolicy.mk
-BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+    BOARD_SEPOLICY_DIRS += \
+    $(COMMON_PATH)/sepolicy
 
 # Shims
 TARGET_LD_SHIM_LIBS := \
