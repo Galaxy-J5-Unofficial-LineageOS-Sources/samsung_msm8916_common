65272296b3fb0ccc2b6829e6eab7dbd487d0f126
David <davidmolinamorales1999@gmail.com>
2021-11-24 22:59:22 +0100

    msm8916-common: define QCOMM manufacturer && SOC model

--- msm8916.mk
+++ msm8916.mk
@@ -268,6 +268,11 @@ PRODUCT_PACKAGES += \
     libkeyutils \
     tcpdump
 
+# QUALCOMM PLATFORM
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.soc.manufacturer=Qualcomm \
+	ro.soc.model=MSM8916
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
