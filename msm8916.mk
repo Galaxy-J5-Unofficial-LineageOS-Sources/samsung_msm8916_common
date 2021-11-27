d2ebcbf469864bb722b42b3be074f59a91a2c574
David <davidmolinamorales1999@gmail.com>
2021-11-27 12:31:14 +0100

    msm8916-common: drop bootimg profiles

--- msm8916.mk
+++ msm8916.mk
@@ -475,10 +475,5 @@ SRC_AUDIO_HAL_DIR := hardware/qcom-caf/msm8916/audio
 PRODUCT_SOONG_NAMESPACES += \
     hardware/qcom-caf/msm8916
 
-# Use a profile based boot image for this device. Note that this is currently a
-# generic profile and not Android Go optimized.
-PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
-
 # Speed profile services and wifi-service to reduce RAM and storage.
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
