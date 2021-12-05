aae353941eb5e19f6c14f65e5178b2cdea942d17
David <davidmolinamorales1999@gmail.com>
2021-12-05 22:00:24 +0100

    msm8916-common: upgrade graphics mapper product 2.0=>2.1

--- msm8916.mk
+++ msm8916.mk
@@ -121,7 +121,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.4-service \
-    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.graphics.mapper@2.1-impl \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
     android.hardware.configstore@1.1-service \
