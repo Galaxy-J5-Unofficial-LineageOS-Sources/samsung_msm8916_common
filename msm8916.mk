ec2ca60d4c4b3d17e529ab469b06211d837f2210
David <davidmolinamorales1999@gmail.com>
2021-12-07 12:40:52 +0100

    msm8916-common: updtae Radio HALs

--- msm8916.mk
+++ msm8916.mk
@@ -364,9 +364,10 @@ PRODUCT_PACKAGES += \
      
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.4.vendor \
-    android.hardware.radio.config@1.2.vendor \
-    android.hardware.radio.deprecated@1.0.vendor
+    android.hardware.radio@1.6.vendor \
+    android.hardware.radio.config@1.3.vendor \
+    android.hardware.radio.deprecated@1.0.vendor \
+    android.hardware.secure_element@1.2.vendor
  
 # NFC
 PRODUCT_PACKAGES += \
