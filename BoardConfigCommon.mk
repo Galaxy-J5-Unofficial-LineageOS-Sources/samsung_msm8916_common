ec5efacd557b878b21e0326178935c3dfd226009
David <davidmolinamorales1999@gmail.com>
2021-12-15 00:19:34 +0100

    msm8916-common: drop SELINUX_IGNORE_NEVERALLOWS := true

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -221,7 +221,6 @@ TARGET_RECOVERY_UI_BLANK_UNBLANK_ON_INIT := true
 TARGET_RECOVERY_QCOM_RTC_FIX	:= true
 
 # SELinux
-SELINUX_IGNORE_NEVERALLOWS := true
 include device/qcom/sepolicy-legacy/sepolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += device/samsung/msm8916-common/sepolicy
 
