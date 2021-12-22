e7fb8f86f219c1517bc8d2d7b0b9f40f8b6b7674
David <davidmolinamorales1999@gmail.com>
2021-12-22 03:01:02 +0100

    msm8916-common: This is redundant and not needed

--- msm8916.mk
+++ msm8916.mk
@@ -459,12 +459,4 @@ PRODUCT_PACKAGES += \
     RemoveGallery2 \
     RemoveTrebuchet \
     RemoveEleven
-    
- # CAF repos
-SRC_MEDIA_HAL_DIR := hardware/qcom-caf/msm8916/media
-SRC_DISPLAY_HAL_DIR := hardware/qcom-caf/msm8916/display
-SRC_AUDIO_HAL_DIR := hardware/qcom-caf/msm8916/audio
-
-PRODUCT_SOONG_NAMESPACES += \
-    hardware/qcom-caf/msm8916
    
