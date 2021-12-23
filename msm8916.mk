cf3107ce45ef959870c2bbdb14a92a6a74a21a16
David <davidmolinamorales1999@gmail.com>
2021-12-23 15:39:17 +0100

    msm8916-common: Restore renderscript HIDL

--- msm8916.mk
+++ msm8916.mk
@@ -122,6 +122,8 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
     android.hardware.configstore@1.1-service \
+    android.hardware.renderscript@1.0-impl \
+    android.hardware.renderscript@1.0-service \
     gralloc.msm8916 \
     hwcomposer.msm8916 \
     libtinyxml \
