f6d3c7375236cd94123605ba928dba706f41fc9e
David <davidmolinamorales1999@gmail.com>
2022-01-02 21:14:01 +0100

    msm8916-common: Build soundtrigger

--- msm8916.mk
+++ msm8916.mk
@@ -72,6 +72,10 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
+    android.hardware.soundtrigger@2.0-impl \
+    android.hardware.soundtrigger@2.1-impl \
+    android.hardware.soundtrigger@2.2-impl \
+    android.hardware.soundtrigger@2.3-impl \
     audio.a2dp.default \
     audio.bluetooth.default \
     audio.primary.msm8916 \
