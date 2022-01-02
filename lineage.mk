f13bcc7fc65fe1f13861c7042e1b93746e527fc2
David <davidmolinamorales1999@gmail.com>
2022-01-02 02:09:05 +0100

    msm8916-common: Unified makefile for LineageOS
    - Includes all system properties

--- lineage.mk
+++ lineage.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017-2018 The LineageOS Project
+# Copyright (C) 2017-2021 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,16 +13,75 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+  
+# Release name
+PRODUCT_RELEASE_NAME := Samsung Galaxy J5
+
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
+# Include Bluetooth config
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
 
 # Inherit from common
 $(call inherit-product, device/samsung/qcom-common/qcom-common.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
 # Include proprietary blobs
 $(call inherit-product-if-exists, vendor/samsung/msm8916-common/msm8916-common-vendor.mk)
 
+# Inherit from vendor
+$(call inherit-product, vendor/samsung/j5-common/j5-common-vendor.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_CHARACTERISTICS := phone
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+
 LOCAL_PATH := device/samsung/msm8916-common
 COMMON_PATH := device/samsung/msm8916-common
+
+# API - Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vndk.version=current
+
+# Assistant - Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.opa.eligible_device=true
+
+# SoC - Properties
+# Defines SoC manufacturer and model
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.soc.manufacturer=Qualcomm \
+	ro.soc.model=MSM8916
+
+# Chipset - Properties
+ifeq ($(TARGET_BOARD_PLATFORM_VARIANT),msm8929)
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.chipname=MSM8929
+else ifeq ($(TARGET_BOARD_PLATFORM_VARIANT),msm8939)
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.chipname=MSM8939
+endif
+
+# Charger - Properties
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.charger.disable_init_blank=true \
+    ro.charger.enable_suspend=true
+
+# Connectivity Engine - Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.cne.dpm=0 \
+    persist.cne.feature=0 \
+    persist.dpm.feature=0
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
@@ -62,27 +121,65 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
+    
+# Audio encoders - Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    qcom.hw.aac.encoder=false
+
+# Audio - Fluence - Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.audio.fluence.speaker=true \
+    persist.vendor.audio.fluence.voicecall=true \
+    persist.vendor.audio.fluence.voicerec=false \
+    ro.vendor.audio.sdk.fluencetype=none \
+    ro.vendor.audio.sdk.ssr=false
+
+# Audio offload - Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.audio.offload.buffer.size.kb=32 \
+    vendor.audio.offload.gapless.enabled=true \
+    audio.offload.min.duration.secs=30 \
+    vendor.audio.offload.track.enable=true \
+    vendor.audio.tunnel.encode=false
+
+# Audio voice recording - Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.voice.path.for.pcm.voip=true \
+    vendor.voice.playback.conc.disabled=true \
+    vendor.voice.record.conc.disabled=true \
+    vendor.voice.voip.conc.disabled=true
 
 ifneq ($(USE_CUSTOM_MIXER_PATHS), true)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
 endif
 
-# Bluetooth S
-PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0.vendor
-
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.1-service \
-
-PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth.audio@2.0-impl \
     android.hardware.bluetooth.a2dp@1.0-impl.mock
+    
+# Bluetooth - Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    bluetooth.hfp.client=1 \
+    ro.bluetooth.dun=true \
+    ro.bluetooth.hfp.ver=1.7 \
+    ro.bluetooth.sap=true \
+    ro.bt.bdaddr_path=/efs/bluetooth/bt_addr \
+    ro.qualcomm.bt.hci_transport=smd \
+    vendor.bluetooth.soc=pronto \
+    vendor.qcom.bluetooth.soc=pronto
 
 PRODUCT_PACKAGES += \
     libbase_shim \
     libbt-vendor
+    
+# Boot - Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    sys.vendor.shutdown.waittime=500 \
+    ro.build.shutdown_timeout=0 
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
@@ -96,6 +193,13 @@ PRODUCT_PACKAGES += \
     libmm-qcamera \
     camera.msm8916 \
     Camera2
+    
+ # Camera - Properties
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    camera2.portability.force_api=1 \
+    camera.disable_treble=true \
+    camera2.portability.force_api=1 \
+    debug.camcorder.disablemeta=true
 
 # Connectivity Engine support
 PRODUCT_PACKAGES += \
@@ -107,12 +211,39 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/data/netmgr_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/data/netmgr_config.xml \
     $(LOCAL_PATH)/configs/data/qmi_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/data/qmi_config.xml
 
+# Dalvik - properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapgrowthlimit=192m \
+    dalvik.vm.heapsize=256m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=512k \
+    dalvik.vm.heapmaxfree=8m
+
+# Data modules - Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.data.netmgrd.qos.enable=false \
+    ro.use_data_netmgrd=false
+
 # Dex
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     TrebuchetQuickStep \
     Settings \
     SystemUI
    
+# Debloater
+PRODUCT_PACKAGES += \
+    RemoveCamera2 \
+    RemoveJely \
+    RemoveCalculator2 \
+    RemoveMessaging \
+    RemoveEtar \
+    RemoveContacts \
+    RemoveRecorder \
+    RemoveGallery2 \
+    RemoveTrebuchet \
+    RemoveEleven
+    
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
@@ -121,11 +252,22 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
+    android.hardware.configstore@1.1-service \
     gralloc.msm8916 \
     hwcomposer.msm8916 \
     libtinyxml \
     libtinyxml2 \
     memtrack.msm8916 
+    
+# Display - Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.sf.enable_gl_backpressure=1 \
+    debug.composition.type=c2d \
+    debug.egl.hw=1 \
+    debug.sf.hw=1 \
+    debug.hwui.use_buffer_age=false \
+    debug.renderengine.backend=threaded \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=1
 
 # DRM
 PRODUCT_PACKAGES += \
@@ -148,6 +290,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     qcom.fmradio \
     FMRadio
+    
+# FM - Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.fm.transmitter=false
     
 # Quick Charge
 PRODUCT_PACKAGES += \
@@ -187,6 +333,13 @@ PRODUCT_PACKAGES += \
     gps.msm8916 \
     libshim_gps \
     liblocadapterbase_shim
+    
+# GPS - Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.gps.qc_nlp_in_use=1 \
+    persist.loc.nlp_name=com.qualcomm.location \
+    ro.gps.agps_provider=1 \
+    ro.pip.gated=0
 
 # Health HAL
 PRODUCT_PACKAGES += \
@@ -256,6 +409,27 @@ PRODUCT_PACKAGES += \
     libOmxQcelp13Enc \
     libOmxVdec \
     libOmxVenc
+    
+ # Media - Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.stagefright.omx_default_rank.sw-audio=1 \
+    debug.stagefright.omx_default_rank=0 \
+    persist.media.treble_omx=false \
+    media.aac_51_output_enabled=true \
+    media.stagefright.enable-aac=true \
+    media.stagefright.enable-fma2dp=true \
+    media.stagefright.enable-http=true \
+    media.stagefright.enable-player=true \
+    media.stagefright.enable-qcp=true \
+    media.stagefright.enable-scan=true \
+    media.stagefright.legacyencoder=true \
+    media.stagefright.less-secure=true \
+    media.stagefright.use-awesome=true \
+    media.swhevccodectype=0 \
+    debug.stagefright.ccodec=0 \
+    mm.enable.qcom_parser=3183219 \
+    mm.enable.smoothstreaming=true \
+    mmp.enable.3g2=true
 
 # Misc
 PRODUCT_PACKAGES += \
@@ -264,10 +438,24 @@ PRODUCT_PACKAGES += \
     libkeyutils \
     tcpdump
     
+# Misc - Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.mdpcomp.logs=0 \
+    dev.pm.dyn_samplingrate=1 \
+    persist.hwc.enable_vds=1 \
+    persist.hwc.mdpcomp.enable=true \
+    persist.sys.storage_preload=1 \
+    ro.data.large_tcp_window_size=true \
+    sys.disable_ext_animation=1
+    
 # Netmgrd
 PRODUCT_PACKAGES += \
     android.system.net.netd@1.1.vendor
-
+    
+# OEM Unlock - Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.oem_unlock_supported=0
+    
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
@@ -289,6 +477,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/handheld_core_hardware.xml
 
+# Perf - Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.extension_library=libqti-perfd-client.so
+
 # Power configuration
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
@@ -298,9 +490,6 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
     android.hardware.power.stats@1.0-service.mock
 
-# Properties
-include $(LOCAL_PATH)/prop.mk
-
 # Radio
 PRODUCT_PACKAGES += \
     libshim_secril \
@@ -308,6 +497,12 @@ PRODUCT_PACKAGES += \
     libxml2 \
     macloader \
     libcutils_shim
+    
+# Radio - Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.radio.add_power_save=1 \
+    persist.radio.apm_sim_not_pwdn=1 \
+    persist.radio.sib16_support=1
 
 # Rootdir
 PRODUCT_PACKAGES += \
@@ -336,6 +531,10 @@ PRODUCT_COPY_FILES += \
 # RRO
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
+    
+# RenderScript HAL
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
 
 # Seccomp
 PRODUCT_COPY_FILES += \
@@ -350,6 +549,29 @@ PRODUCT_PACKAGES += \
         libril \
 	librilutils \
 	rild
+
+# RIL - Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.rild.nitz_long_ons_0="" \
+    persist.rild.nitz_long_ons_1="" \
+    persist.rild.nitz_long_ons_2="" \
+    persist.rild.nitz_long_ons_3="" \
+    persist.rild.nitz_plmn="" \
+    persist.rild.nitz_short_ons_0="" \
+    persist.rild.nitz_short_ons_1="" \
+    persist.rild.nitz_short_ons_2="" \
+    persist.rild.nitz_short_ons_3="" \
+    ril.subscription.types=NV,RUIM \
+    DEVICE_PROVISIONED=1 \
+    rild.libpath=/vendor/lib/libsec-ril.so \
+    ro.multisim.set_audio_params=true
+
+# SAMP SPCM - Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    sys.config.samp_spcm_enable=true \
+    sys.config.spcm_db_enable=true \
+    sys.config.spcm_db_launcher=true \
+    sys.config.spcm_preload_enable=true
 
 # Security configuration file
 PRODUCT_COPY_FILES += \
@@ -405,6 +627,15 @@ PRODUCT_COPY_FILES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
+    
+# Time services - Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.timed.enable=true
+    
+# TWRP - Properties
+# This patch applies to Android S+
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.binary_xml=false
 
 # USB HAL
 PRODUCT_PACKAGES += \
@@ -418,6 +649,15 @@ PRODUCT_PACKAGES += \
 # Vendor service manager
 PRODUCT_PACKAGES += \
     vndservicemanager
+
+# Vendor security patch level - Properties
+# Defines latest vendor update from OEM
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.lineage.build.vendor_security_patch=2017-09-01
+    
+# Video encoding - Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    vidc.enc.narrow.searchrange=1
 
 # Wifi configuration files
 PRODUCT_COPY_FILES += \
@@ -433,6 +673,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     com.android.media.remotedisplay \
     com.android.media.remotedisplay.xml
+    
+# WiDi - Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.debug.wfd.enable=1 \
+    persist.sys.wfd.virtual=0
 
 # Wifi
 PRODUCT_PACKAGES += \
@@ -445,16 +690,7 @@ PRODUCT_PACKAGES += \
     wificond \
     wpa_supplicant
     
-# Remove unwanted packages
-PRODUCT_PACKAGES += \
-    RemoveCamera2 \
-    RemoveJely \
-    RemoveCalculator2 \
-    RemoveMessaging \
-    RemoveEtar \
-    RemoveContacts \
-    RemoveRecorder \
-    RemoveGallery2 \
-    RemoveTrebuchet \
-    RemoveEleven
+# ZRAM - Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.zram.size=128
    
