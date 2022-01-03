624a5f085960c85dbb0b71e753bac8aa1a9e1bff
David <davidmolinamorales1999@gmail.com>
2022-01-03 15:45:05 +0100

    msm8916-common: Drop secure keymaster @3.0 since we're using software implementation
    Could not find any keystore module, using software-only implementation.
    No secure Keymaster implementation found, but device offers insecure Keymaster HAL. Using as default.

--- msm8916.mk
+++ msm8916.mk
@@ -351,8 +351,6 @@ PRODUCT_COPY_FILES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl \
-    android.hardware.keymaster@3.0-service \
     android.hardware.keymaster@4.1.vendor \
     android.hardware.keymaster@4.1-service
 
