520d76ef650ea159ad89b4cd6272e83ffe8ed8dc
David <davidmolinamorales1999@gmail.com>
2022-02-02 13:40:11 +0100

    msm8916-common: Makefile: Retore SE
    01-24 20:58:25.611  1460  1460 F DEBUG   : Build fingerprint: 'samsung/lineage_j5nlte/j5nlte:12/SQ1D.220105.007/eng.davidm.20220124.153418:userdebug/test-keys'
    01-24 20:58:25.611  1460  1460 F DEBUG   : Revision: '5'
    01-24 20:58:25.611  1460  1460 F DEBUG   : ABI: 'arm'
    01-24 20:58:25.611  1460  1460 F DEBUG   : Timestamp: 2022-01-24 20:58:25.173702623+0000
    01-24 20:58:25.611  1460  1460 F DEBUG   : Process uptime: 0s
    01-24 20:58:25.612  1460  1460 F DEBUG   : Cmdline: /vendor/bin/hw/android.hardware.secure_element@1.0-service-disabled
    01-24 20:58:25.612  1460  1460 F DEBUG   : pid: 1434, tid: 1434, name: secure_element@  >>> /vendor/bin/hw/android.hardware.secure_element@1.0-service-disabled <<<

--- msm8916.mk
+++ msm8916.mk
@@ -439,7 +439,6 @@ PRODUCT_PACKAGES += \
     android.hardware.secure_element@1.1.vendor \
     android.hardware.secure_element@1.2 \
     android.hardware.secure_element@1.2.vendor \
-    android.hardware.secure_element@1.0-service-disabled \
     
 # NFC
 PRODUCT_PACKAGES += \
