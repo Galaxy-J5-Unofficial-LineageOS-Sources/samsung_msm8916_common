2bbe7db17f3c7dd0b0010ebb6b96e0e1549da84f
David <davidmolinamorales1999@gmail.com>
2022-02-02 10:05:23 +0100

    msm8916-common: Makefile: Build android.hardware.bluetooth @1.1 vendor version
    I/hwservicemanager(482): getTransport: Cannot find entry android.hardware.bluetooth@1.1::IBluetoothHci/default in either framework or device VINTF manifest.
    I/hwservicemanager(482): Since android.hardware.bluetooth@1.0::IBluetoothHci/default is not registered, trying to start it as a lazy HAL.
    I/HidlServiceManagement(15065): getService: Trying again for android.hardware.bluetooth@1.0::IBluetoothHci/default...

--- msm8916.mk
+++ msm8916.mk
@@ -95,6 +95,7 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl \
     android.hardware.bluetooth@1.0-service \
     android.hardware.bluetooth@1.0.vendor \
+    android.hardware.bluetooth@1.1.vendor \
     android.hardware.bluetooth.audio@2.1 \
     android.hardware.bluetooth.audio@2.1-impl \
     android.hardware.bluetooth.a2dp@1.0-impl.mock \
