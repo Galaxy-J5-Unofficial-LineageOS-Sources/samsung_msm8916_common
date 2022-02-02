e067d308b8ba56bb513a20c8330074237a7a7a7a
David <davidmolinamorales1999@gmail.com>
2022-02-02 12:05:00 +0100

    msm8916-common: Makefile: disable android.hardware.secure_element

--- msm8916.mk
+++ msm8916.mk
@@ -436,7 +436,8 @@ PRODUCT_PACKAGES += \
     android.hardware.secure_element@1.1 \
     android.hardware.secure_element@1.1.vendor \
     android.hardware.secure_element@1.2 \
-    android.hardware.secure_element@1.2.vendor    
+    android.hardware.secure_element@1.2.vendor \
+    android.hardware.secure_element@1.0-service-disabled \
     
 # NFC
 PRODUCT_PACKAGES += \
