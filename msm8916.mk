a9e3c3c3300f6f5586f74ee88a846b08c9c6b17c
David <davidmolinamorales1999@gmail.com>
2022-02-02 23:22:58 +0100

    msm8916-common: Makefile: Update products

--- msm8916.mk
+++ msm8916.mk
@@ -93,10 +93,11 @@ endif
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl \
-    android.hardware.bluetooth@1.0-service \
-    android.hardware.bluetooth@1.0-impl-1.1 \
     android.hardware.bluetooth@1.1-impl \
+    android.hardware.bluetooth@1.0-service \
+    android.hardware.bluetooth@1.0 \
     android.hardware.bluetooth@1.0.vendor \
+    android.hardware.bluetooth@1.1 \
     android.hardware.bluetooth@1.1.vendor \
     android.hardware.bluetooth.audio@2.1 \
     android.hardware.bluetooth.audio@2.1-impl \
