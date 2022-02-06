0f07da14dce6436699ee3d94f144227114fffe3d
David <davidmolinamorales1999@gmail.com>
2022-02-06 23:32:18 +0100

    msm8916-common: fix typo

--- msm8916.mk
+++ msm8916.mk
@@ -96,7 +96,7 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.a2dp@1.0.vendor\
     android.hardware.bluetooth.audio@2.1 \
     android.hardware.bluetooth.audio@2.1-impl \
-    android.hardware.bluetooth.a2dp@1.0-impl.mock 
+    android.hardware.bluetooth.a2dp@1.0-impl.mock \
     libbase_shim \
     libbase-v28 \
     libbt-vendor
