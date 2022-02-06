01e1b769072b865e7d7bd34eeff0f4243a9b5ecf
David <davidmolinamorales1999@gmail.com>
2022-02-06 23:08:32 +0100

    msm8916-common: Bluetooth: Drop  default service, use lazy service instead

--- msm8916.mk
+++ msm8916.mk
@@ -92,9 +92,6 @@ endif
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0.vendor \
-    android.hardware.bluetooth@1.0-impl \
-    android.hardware.bluetooth@1.0-service \
     android.hardware.bluetooth.a2dp@1.0.vendor\
     android.hardware.bluetooth.audio@2.1 \
     android.hardware.bluetooth.audio@2.1-impl \
