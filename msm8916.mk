3290067ca9a604c6bf7e9c634c4183d695c8a46f
David <davidmolinamorales1999@gmail.com>
2022-02-08 14:34:05 +0100

    msm8916-common: Makefile: Update paths for GPS configs

--- msm8916.mk
+++ msm8916.mk
@@ -199,14 +199,13 @@ PRODUCT_COPY_FILES += \
 
 # GPS Configs
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/gps/apdr.conf:system/vendor/etc/apdr.conf \
-    $(LOCAL_PATH)/gps/flp.conf:system/vendor/etc/flp.conf \
-    $(LOCAL_PATH)/gps/gps.conf:system/vendor/etc/gps.conf \
-    $(LOCAL_PATH)/gps/izat.conf:system/vendor/etc/izat.conf \
-    $(LOCAL_PATH)/gps/lowi.conf:system/vendor/etc/lowi.conf \
-    $(LOCAL_PATH)/gps/sap.conf:system/vendor/etc/sap.conf \
-    $(LOCAL_PATH)/gps/xtwifi.conf:system/vendor/etc/xtwifi.conf
-
+    $(LOCAL_PATH)/configs/gps/apdr.conf:system/vendor/etc/apdr.conf \
+    $(LOCAL_PATH)/configs/gps/flp.conf:system/vendor/etc/flp.conf \
+    $(LOCAL_PATH)/configs/gps/gps.conf:system/vendor/etc/gps.conf \
+    $(LOCAL_PATH)/configs/gps/izat.conf:system/vendor/etc/izat.conf \
+    $(LOCAL_PATH)/configs/gps/lowi.conf:system/vendor/etc/lowi.conf \
+    $(LOCAL_PATH)/configs/gps/sap.conf:system/vendor/etc/sap.conf \
+    $(LOCAL_PATH)/configs/gps/xtwifi.conf:system/vendor/etc/xtwifi.conf
 
 # GPS
 PRODUCT_PACKAGES += \
