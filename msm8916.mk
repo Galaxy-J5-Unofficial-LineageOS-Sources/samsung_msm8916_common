d01342a8972360db455b2964a12345c2dfcc4dd0
David <davidmolinamorales1999@gmail.com>
2022-02-10 09:59:36 +0100

    msm8916-common: Makefile: Switch to lazy clearkey service

--- msm8916.mk
+++ msm8916.mk
@@ -158,9 +158,8 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service \
-    android.hardware.drm@1.4-service.clearkey \
-    android.hardware.drm@1.4.vendor
+    android.hardware.drm@1.0-service-lazy \
+    android.hardware.drm@1.4-service-lazy.clearkey
 
 # Protobuf
 PRODUCT_PACKAGES += \
