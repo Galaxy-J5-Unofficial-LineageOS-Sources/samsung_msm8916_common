be3b3455a182ddde0984a2f3a1dec49d53fc3de2
David <davidmolinamorales1999@gmail.com>
2022-02-11 21:55:42 +0100

    msm8916-common: Makefile: Fix weird space that breaks system prop generation

--- msm8916.mk
+++ msm8916.mk
@@ -105,7 +105,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
 
 # BPF
-PRODUCT_PROPERTY_OVERRIDES += \     
+PRODUCT_PROPERTY_OVERRIDES += \
     ro.kernel.ebpf.supported=false
 
 # Camera
