09165c522bb609ce8d698fbb86e0f1411a45d8f3
David <davidmolinamorales1999@gmail.com>
2022-02-12 00:16:23 +0100

    msm8916-common: Bluetooth: Build libldacBT_bco
     E a2dp_vendor_ldac_decoder: system/bt/stack/a2dp/a2dp_vendor_ldac_decoder.cc:134 A2DP_VendorLoadDecoderLdac: A2DP_VendorLoadDecoderLdac: cannot open LDAC decoder library libldacBT_bco.so: dlopen failed: library "libldacBT_bco.so" not found

--- msm8916.mk
+++ msm8916.mk
@@ -93,6 +93,7 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.1 \
     android.hardware.bluetooth.audio@2.1-impl \
     android.hardware.bluetooth.a2dp@1.0-impl.mock \
+    libldacBT_bco \
     libbase_shim \
     libbase-v28 \
     libbt-vendor
