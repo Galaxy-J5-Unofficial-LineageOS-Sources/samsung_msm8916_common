a455194784424f44f95fea35aba13812cb3e6f75
David <davidmolinamorales1999@gmail.com>
2022-02-13 00:05:49 +0100

    msm8916-common: property: Drop duplicated declaration
    error: found duplicate sysprop assignments:
    ro.telephony.call_ring.multiple=false
    ro.telephony.call_ring.multiple=0

--- device.mk
+++ device.mk
@@ -23,8 +23,7 @@ TARGET_FLATTEN_APEX := true
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.product.first_api_level=21 \
     ro.vndk.version=current \
-    ro.telephony.iwlan_operation_mode=legacy \
-    ro.telephony.call_ring.multiple=false
+    ro.telephony.iwlan_operation_mode=legacy 
 
 # Include Bluetooth config
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
