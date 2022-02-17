41dd31bf63dbdfef3216fda5de3c57615962ce36
daviiid99 <daviiideveloper@gmail.com>
2022-02-17 13:36:39 +0100

    msm8916-common: fastcharge: Build FastCharge HAL

--- msm8916.mk
+++ msm8916.mk
@@ -149,6 +149,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fs_config_files
 
+# FastCharge
+PRODUCT_PACKAGES += \
+    vendor.lineage.fastcharge@1.0-service.usb
+
 # FS
 PRODUCT_PACKAGES += \
     fsck.f2fs
