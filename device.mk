d87ab198633f45c453aca28e7365dfc873f4c348
David <davidmolinamorales1999@gmail.com>
2022-02-18 11:11:29 +0100

    msm8916-common: Makefile: Drop SKIP_BOOT_JARS_CHECK

--- device.mk
+++ device.mk
@@ -13,9 +13,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# The target has no boot jars to check
-SKIP_BOOT_JARS_CHECK := true
-
 # APEX
 TARGET_FLATTEN_APEX := true
 
