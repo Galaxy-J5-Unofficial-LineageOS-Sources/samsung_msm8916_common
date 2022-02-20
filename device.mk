5d20b17af9f4b9f1aa885906a5d6c0ca764e3746
David <davidmolinamorales1999@gmail.com>
2022-02-20 21:42:22 +0100

    Reverts 'msm8916-common: apex: Remove updatable_apex && turn off compressed apex'

--- device.mk
+++ device.mk
@@ -13,8 +13,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# APEX
-TARGET_FLATTEN_APEX := true
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
+# Apex
+PRODUCT_COMPRESSED_APEX := false
 
 # Include Bluetooth config
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
