ca271ec6a3e3180f988994a360d865d8834f0a04
David <davidmolinamorales1999@gmail.com>
2022-02-20 23:05:23 +0100

    msm8916-common: recovery: Testing a specific defconfig for los recovery building

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -147,6 +147,7 @@ TARGET_KERNEL_CONFIG := msm8916_sec_defconfig
 TARGET_KERNEL_SELINUX_CONFIG := selinux_defconfig
 TARGET_KERNEL_SELINUX_LOG_CONFIG := selinux_log_defconfig
 TARGET_KERNEL_SOURCE := kernel/samsung/msm8916
+TARGET_KERNEL_RECOVERY_CONFIG := j5nlte_recovery_defconfig
 TARGET_HAS_MEMFD_BACKPORT := true
 
 # Kernel - Toolchain
