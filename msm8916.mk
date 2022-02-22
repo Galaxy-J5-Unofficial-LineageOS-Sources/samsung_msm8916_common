00a3172348df2ea1eee6d02e217cc14e86531aaa
David <davidmolinamorales1999@gmail.com>
2022-02-22 12:57:56 +0100

    msm8916-common: Drop dead targets

--- msm8916.mk
+++ msm8916.mk
@@ -222,14 +222,11 @@ PRODUCT_PACKAGES += \
 # HIDL 
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.manager@1.0 \
-    android.hidl.manager@1.0-java
-
+    android.hidl.manager@1.0 
+    
 PRODUCT_PACKAGES += \
     libhidltransport \
-    libhidltransport.vendor \
     libhwbinder \
-    libhwbinder.vendor \
     libjson \
     libjson.vendor 
 
