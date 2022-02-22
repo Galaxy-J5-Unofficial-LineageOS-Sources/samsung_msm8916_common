4cdc0f67b3f754b2a65e243794ca81e8e6256e5d
David <davidmolinamorales1999@gmail.com>
2022-02-22 23:29:17 +0100

    msm8916-common: APEX: Use flatten APEX
    Our legacy 3.10 kernel doesn't support updatable apex

--- device.mk
+++ device.mk
@@ -13,11 +13,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
-# Apex
-PRODUCT_COMPRESSED_APEX := false
+# Enable flatten APEX
+TARGET_FLATTEN_APEX := true
 
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
