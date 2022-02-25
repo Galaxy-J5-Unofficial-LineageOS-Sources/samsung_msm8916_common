47b543d6fded1405f2058c1ca6295ae45a4fc3eb
daviiid99 <daviiideveloper@gmail.com>
2022-02-25 18:50:13 +0100

    msm8916-common: Vibrator: Kang Vibrator HAL from Lenovo A6010                 Credits to @acroreiser

--- msm8916.mk
+++ msm8916.mk
@@ -479,8 +479,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
+    android.hardware.vibrator@1.1-service.msm8916
     
 # Vendor service manager
 PRODUCT_PACKAGES += \
