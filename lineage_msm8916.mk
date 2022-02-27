48691d54e7f3ede5e498e13f70dd950086f70546
David <davidmolinamorales1999@gmail.com>
2022-02-27 20:20:22 +0100

    msm8916-common: Sensors: Build sensorservice 1.0 HAL

--- lineage_msm8916.mk
+++ lineage_msm8916.mk
@@ -411,6 +411,7 @@ PRODUCT_COPY_FILES += \
 # Sensor HAL
 PRODUCT_PACKAGES += \
     libsensorndkbridge \
+    android.frameworks.sensorservice@1.0 \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service \
     android.hardware.sensors@2.0 \
