85ab4cc80bcf72c68ac514f08766f087b74b9bc4
David <davidmolinamorales1999@gmail.com>
2022-03-01 17:30:24 +0100

    msm8916-common: Drop android.frameworks.sensorservice@1.0, broken
    04-11 17:47:45.036   225   225 I hwservicemanager: Since android.frameworks.sensorservice@1.0::ISensorManager/default is not registered, trying to start it as a lazy HAL.
    04-11 17:47:45.036   562   588 I HidlServiceManagement: getService: Trying again for android.frameworks.sensorservice@1.0::ISensorManager/default...
    04-11 17:47:45.039   225  1204 W libc    : Unable to set property "ctl.interface_start" to "android.frameworks.sensorservice@1.0::ISensorManager/default": error code: 0x20
    04-11 17:47:46.906     0     0 E [3:           init:    1] init: Control message: Could not find 'android.frameworks.sensorservice@1.0::ISensorManager/default' for ctl.interface_start from pid: 225 (/system/bin/hwservicemanager)
    04-11 17:47:45.039   225  1204 I hwservicemanager: Tried to start android.frameworks.sensorservice@1.0::ISensorManager/default as a lazy service, but was unable to. Usually this happens when a service is not installed, but if the service is intended to be used as a lazy service, then it may be configured incorrectly.
    04-11 17:47:45.046  1097  1097 D CompatibilityChangeReporter: Compat change id reported: 143539591; UID -1; state: ENABLED
    04-11 17:47:45.055   562   562 W HidlServiceManagement: Waited one second for android.frameworks.sensorservice@1.0::ISensorManager/default
    04-11 17:47:45.056   225   225 I hwservicemanager: Since android.frameworks.sensorservice@1.0::ISensorManager/default is not registered, trying to start it as a lazy HAL.
    04-11 17:47:45.056   562   562 I HidlServiceManagement: getService: Trying again for android.frameworks.sensorservice@1.0::ISensorManager/default...
    04-11 17:47:45.062  1097  1097 I PackageManager: /system/product/overlay/IconShapePebble changed; collecting certs
    04-11 17:47:45.074   225  1205 W libc    : Unable to set property "ctl.interface_start" to "android.frameworks.sensorservice@1.0::ISensorManager/default": error code: 0x20
    04-11 17:47:45.074   225  1205 I hwservicemanager: Tried to start android.frameworks.sensorservice@1.0::ISensorManager/default as a lazy service, but was unable to. Usually this happens when a service is not installed, but if the service is intended to be used as a lazy service, then it may be configured incorrectly.

--- lineage_msm8916.mk
+++ lineage_msm8916.mk
@@ -392,12 +392,8 @@ PRODUCT_COPY_FILES += \
 # Sensor HAL
 PRODUCT_PACKAGES += \
     libsensorndkbridge \
-    android.frameworks.sensorservice@1.0 \
-    android.frameworks.sensorservice@1.0.vendor \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service \
-    android.hardware.sensors@2.0 \
-    android.hardware.sensors@2.1 \
     sensors.msm8916
     
 # Thermal HAL
