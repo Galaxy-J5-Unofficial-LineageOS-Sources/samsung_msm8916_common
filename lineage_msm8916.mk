4bc1a3986867a5ccd03adbc5ab09a9b6901b2840
David <davidmolinamorales1999@gmail.com>
2022-03-05 18:29:36 +0100

    msm8916-common: Build  android.hardware.graphics.allocator@3.0-service
    I hwservicemanager: getTransport: Cannot find entry android.hardware.graphics.allocator@3.0::IAllocator/default in either framework or device VINTF manifest.
    E LegacySupport: Could not get passthrough implementation for android.hardware.graphics.allocator@3.0::IAllocator/default.

--- lineage_msm8916.mk
+++ lineage_msm8916.mk
@@ -130,6 +130,7 @@ PRODUCT_PACKAGES += \
     android.hidl.allocator@1.0.vendor \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.allocator@3.0-service \
     android.hardware.graphics.allocator@3.0-impl \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
