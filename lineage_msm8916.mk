098d597228b09d595baef258d3a2337c408b12cd
David <davidmolinamorales1999@gmail.com>
2022-03-10 10:19:52 +0100

    msm8916-common: Provide android.hardware.radio@1.4.vendor on vendor
    03-07 10:26:00.213  1734  1734 W HidlServiceManagement: Waited one second for android.hardware.radio@1.6::IRadio/slot1
    03-07 10:26:00.214   225   225 I hwservicemanager: Since android.hardware.radio@1.6::IRadio/slot1 is not registered, trying to start it as a lazy HAL.
    03-07 10:26:00.214  1734  1734 I HidlServiceManagement: getService: Trying again for android.hardware.radio@1.6::IRadio/slot1...
    03-07 10:26:00.218   225  2170 W libc    : Unable to set property "ctl.interface_start" to "android.hardware.radio@1.6::IRadio/slot1": error code: 0x20

--- lineage_msm8916.mk
+++ lineage_msm8916.mk
@@ -350,6 +350,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.2-radio-service \
     android.hardware.radio.config@1.0-service \
+    android.hardware.radio@1.6.vendor \
     libshim_secril \
     libshim_ril \
     libxml2 \
