f4327f435e16f04fefe06ee0157a7f84ebbe0734
David <davidmolinamorales1999@gmail.com>
2022-03-17 18:18:29 +0100

    msm8916-common: GPS: Address missing SHIM for GNSS
    E AndroidRuntime: java.lang.UnsatisfiedLinkError: dlopen failed: cannot locate symbol "_ZN7android8hardware4gnss4V1_08toStringENS2_15IGnssNiCallback20GnssUserResponseTypeE" referenced by "/system/vendor/lib/vendor.qti.gnss@1.0.so"...

--- libshims/gps/gps_shim.cpp
+++ libshims/gps/gps_shim.cpp
@@ -22,4 +22,4 @@ get_process_name(void) {
 }
 
 extern "C" void _ZN7android8hardware4gnss4V1_08toStringINS2_15IGnssNiCallback17GnssNiNotifyFlagsEEENSt3__112basic_stringIcNS6_11char_traitsIcEENS6_9allocatorIcEEEEj() {}
-
+extern "C" void _ZN7android8hardware4gnss4V1_08toStringENS2_15IGnssNiCallback20GnssUserResponseTypeE() {}
