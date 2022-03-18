590eb0b33e88abca7d9846580fb0a97a3a157cdf
David <davidmolinamorales1999@gmail.com>
2022-03-18 15:56:13 +0100

    msm8916-common :

--- lineage_msm8916.mk
+++ lineage_msm8916.mk
@@ -232,7 +232,7 @@ PRODUCT_PACKAGES += \
     libshim_gps \
     liblocadapterbase_shim
     
-#PRODUCT_COPY_FILES += \
+PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/flp.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/flp.conf \
     $(LOCAL_PATH)/configs/gps/gps.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/gps.conf \
     $(LOCAL_PATH)/configs/gps/izat.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/izat.conf \
