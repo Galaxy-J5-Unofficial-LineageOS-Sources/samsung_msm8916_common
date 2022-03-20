8e4aad8a3c7ae61197f4c218c2fb1f45bbe6babf
David <davidmolinamorales1999@gmail.com>
2022-03-20 07:22:56 +0100

    msm8916-common: Remove no longer needed Bluetooth products

--- lineage_msm8916.mk
+++ lineage_msm8916.mk
@@ -88,11 +88,6 @@ endif
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0.vendor \
-    android.hardware.bluetooth.a2dp@1.0.vendor\
-    android.hardware.bluetooth.audio@2.1 \
-    android.hardware.bluetooth.audio@2.1-impl \
-    android.hardware.bluetooth.a2dp@1.0-impl.mock \
     libbase_shim \
     libbase-v28 \
     libbt-vendor \
