8475b365f1604ae77a40fe0c4e26153745bbbc8d
David <davidmolinamorales1999@gmail.com>
2022-03-27 21:15:28 +0200

    msm8916-common: Camera: Update products

--- lineage_msm8916.mk
+++ lineage_msm8916.mk
@@ -99,11 +99,11 @@ PRODUCT_COPY_FILES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
     camera.device@1.0-impl \
     camera.msm8916 \
-    libc_util \
-    libboringssl-compat \
     libcamera_shim \
+    libmm-qcamera \
     Camera2 \
     SnapdragonCamera
 
