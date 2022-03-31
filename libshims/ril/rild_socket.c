30f337a4e8e082964d919103b2ad0a9cd684be8c
David <davidmolinamorales1999@gmail.com>
2022-03-31 19:52:37 +0200

    msm8916-common: RIL : New Shim for netmgrd
    E LocSvc_ApiV02: E/virtual int LocApiV02::initDataServiceClient():3288]: failed to load library libloc_ds_api.so; error=dlopen failed: cannot locate symbol "ds_log_mask" referenced by "/system/vendor/lib/libdsi_netctrl.so"...

--- libshims/ril/rild_socket.c
+++ libshims/ril/rild_socket.c
@@ -24,3 +24,4 @@ void qmi_qos_process_get_primary_granted_qos_req() {}
 void qmi_qos_process_get_secondary_granted_qos_req() {}
 void qmi_qos_srvc_extract_return_code() {}
 void qmi_qos_process_get_granted_qos_resp() {}
+void ds_log_mask() {}
