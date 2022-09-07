bb1a320f911c6db6d563947d6dbca6412a149632
daviiid99 <daviiideveloper@gmail.com>
2022-09-07 16:08:04 +0200

    msm8916-common: Uprev audio HAL to 7.1 Android 13

--- lineage_msm8916.mk
+++ lineage_msm8916.mk
@@ -60,6 +60,7 @@ PRODUCT_PACKAGES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl \
+    android.hardware.audio@7.1-impl \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
     android.hardware.soundtrigger@2.0-impl \
