f431adfe26932a2206fa2ac636f6f8ec1348fbab
David <davidmolinamorales1999@gmail.com>
2022-09-08 00:50:51 +0200

    msm8916-common: wifi: Don't build legacy wifi service
    Fixes hotspot on Android 11+

--- msm8916.mk
+++ msm8916.mk
@@ -430,7 +430,7 @@ PRODUCT_PACKAGES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service.legacy \
+    android.hardware.wifi@1.0-service \
     hostapd \
     hostapd_cli \
     libwpa_client \
