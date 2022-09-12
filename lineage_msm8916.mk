ff806d34ce797c846bde72bb3426f8e01f554260
daviiid99 <daviiideveloper@gmail.com>
2022-09-12 12:37:15 +0200

    msm8916-common: Restore legacy service for wifi

--- lineage_msm8916.mk
+++ lineage_msm8916.mk
@@ -552,7 +552,7 @@ PRODUCT_PACKAGES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi@1.0-service.legacy \
     android.hardware.wifi@1.5.vendor \
     hostapd \
     hostapd_cli \
