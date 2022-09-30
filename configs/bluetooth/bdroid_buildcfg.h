ba1365a9c198d8e45ba2aa52b0d740de4cd7bdf5
David <davidmolinamorales1999@gmail.com>
2022-09-30 23:09:21 +0200

    msm8916-common: bluetooth : Replace BTM_DEF_LOCAL_NAME with a prop

--- configs/bluetooth/bdroid_buildcfg.h
+++ configs/bluetooth/bdroid_buildcfg.h
@@ -16,8 +16,6 @@
 
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
-
-#define BTM_DEF_LOCAL_NAME   "Samsung Galaxy J5"
 #define BLUETOOTH_QTI_SW TRUE
 #define BTC_INCLUDED TRUE
 // Disables read remote device feature
