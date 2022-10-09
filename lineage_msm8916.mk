225ab0ef6cfeaff67f3f5b02f730d67c241fa127
David <davidmolinamorales1999@gmail.com>
2022-10-09 16:33:22 +0200

    msm8916-common: Build Aperture camera app

--- lineage_msm8916.mk
+++ lineage_msm8916.mk
@@ -130,7 +130,8 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0.vendor \
     camera.msm8916 \
     libcamera_shim \
-    libmm-qcamera
+    libmm-qcamera \
+    Aperture
 
 # Connectivity Engine support
 PRODUCT_PACKAGES += \
